1bb8b1f79e2b251463022752db9895514343de44
Storm-77 <czechu600@gmail.com>
2023-10-19 09:28:58 +0200

    login view

--- index.js
+++ index.js
@@ -13,16 +13,28 @@ function sendError(res) {
 const server = http.createServer(async function (req, res) {
 
     if (req.method === "GET") {
+        console.log(req.url)
         if (req.url === "/") {
             const html = await fs.readFile("views/index.html", { encoding: "utf-8" });
             res.setHeader('Content-Type', 'text/html');
             res.end(html);
 
-        } else {
+        } 
+        else if (req.url === "/login") {
+            const html = await fs.readFile("views/login.html", { encoding: "utf-8" });
+            res.setHeader('Content-Type', 'text/html');
+            res.end(html);
+
+        }
+
+        else {
             sendError(res);
         }
     }
 
+    if (req.method === "POST") {
+       console.log(req.url) 
+    }
 });
 
 server.listen(8000, () => {
